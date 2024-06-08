d937ffc3a3444e32f456ffce9b1ab6acceeaf54c
sqlxoxo <stasik.buberenko@gmail.com>
2024-06-08 15:59:04 +0300

    1.3
    CardMove added

--- SimplestCardGame/Assets/Scripts/CardScr.cs
+++ SimplestCardGame/Assets/Scripts/CardScr.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CardScr : MonoBehaviour
+public class CardScr : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    Camera MainCamera;
+    Vector3 offSet;
+
+    Transform DefaultParent;
+
+    void Awake()
     {
-        
+        MainCamera = Camera.allCameras[0];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        offSet = transform.position - MainCamera.ScreenToWorldPoint(eventData.position);
+        DefaultParent = transform.parent;
+
+        transform.SetParent(DefaultParent.parent);
     }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newPos = MainCamera.ScreenToWorldPoint(eventData.position);
+        newPos.z = 0;
+        transform.position = newPos + offSet;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(DefaultParent);
+    }
+
 }
